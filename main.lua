4e305f0a1d8738750d6b6cd2b8f3f71ff1533144
Ethan Bellem <ethan.bellem@ocsbstudent.ca>
2018-02-08 14:22:39 -0500

    Initial commit

---
+++ main.lua
@@ -0,0 +1,13 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+--Created by Ethan
+--Created on Feb 2018
+--
+--shows image in corona simulator
+-----------------------------------------------------------------------------------------
+
+display.setDefault( "background", 0, 2, .5 )
+local image = display.newImageRect( "Zombie Boi.png", 1500, 1500 )
+image:translate(1000, 600)
