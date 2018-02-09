660c3a0861c149fe8cfec9be92d9ba81fdb845f4
Ethan Bellem <ethan.bellem@ocsbstudent.ca>
2018-02-09 12:16:11 -0500

    Background
    i put an apocalypse background on my zombie picture

--- main.lua
+++ main.lua
@@ -9,5 +9,7 @@
 -----------------------------------------------------------------------------------------
 
 display.setDefault( "background", 0, 2, .5 )
+local zeeno = display.newImageRect( "apocalypse.png", 2200, 1500)
+zeeno:translate(1000, 600)
 local image = display.newImageRect( "Zombie Boi.png", 1500, 1500 )
 image:translate(1000, 600)
